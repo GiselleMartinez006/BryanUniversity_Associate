5bbcc0dcef457116ed4586cdfc888157c5ea6ff8
GiselleMartinez006 <53924501+GiselleMartinez006@users.noreply.github.com>
2020-06-03 23:41:33 -0400

    week1

--- ASF-200/Week1/Input/input.py
+++ ASF-200/Week1/Input/input.py
@@ -1,3 +1,7 @@
-from week1.print.message import city, zipcode
-
+import sys
+# insert at 1, 0 is the script path (or '' in REPL)
+sys.path.insert(
+    0, 'c:\\Users\\17865\\dev\\BryanUniversity_Associate\\ASF-200\\week1\\print')
+# from week1.print.message import city, zipcode
+from message import city, zipcode
 print("userCity: "+city + " " + "userZipcode: " + zipcode)
