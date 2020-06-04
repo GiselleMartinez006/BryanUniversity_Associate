07ab3add1bce171edd16ad2e7eff8980184f835b
GiselleMartinez006 <53924501+GiselleMartinez006@users.noreply.github.com>
2020-06-03 21:21:23 -0400

    week1

---
+++ ASF-200/Week1/Input/input.py
@@ -0,0 +1,3 @@
+from week1.print.message import city, zipcode
+
+print("userCity: "+city + " " + "userZipcode: " + zipcode)
---
+++ ASF-200/Week1/string/string.py
@@ -0,0 +1,15 @@
+
+# The string.py file should ask a user their “name”
+#  and “age”
+# In the input.py file,
+# write a code that Print out a message addressed
+#  to them that tells them the year that they will
+#  turn 100 years old.
+name = input("What is your name? ")
+yearsBeforeHundred = 100 - int(input("What is your age? "))
+msg = name + " " + \
+    "will turn 100 years old after " + \
+    str(yearsBeforeHundred) + \
+    " years; unless he/she dies before the year " + \
+    str(2020 + yearsBeforeHundred) + "."
+print(msg)
