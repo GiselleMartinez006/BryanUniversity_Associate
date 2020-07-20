df1f801f0041e5e4159b3bfac054aa0759696fbf
GiselleMartinez006 <53924501+GiselleMartinez006@users.noreply.github.com>
2020-07-20 02:14:31 -0400

    week7 slack

--- ASF-200/week7/capEnv/01/capstone/capstone/urls.py
+++ ASF-200/week7/capEnv/01/capstone/capstone/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 from traveler import views
+from django.contrib.auth.views import LoginView, LogoutView
 admin.autodiscover()
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.IndexView.as_view()),
-    path('<int:pk>/', views.DetailView.as_view())
+    path('', views.IndexView.as_view(), name='homepage'),
+    path('accounts/profile/', views.IndexView.as_view(), name='homepage'),
+    path('<int:pk>/', views.DetailView.as_view(), name='details'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
 ]
 
 
